79dd09a900f9aef4fdb9c5f80c692d98e4747bdf
laviniamarante <laviinia.amarante@gmail.com>
2025-07-28 09:11:20 -0300

    Adiciona projeto com HTML

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 6000;
+const PORT = 3000;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 let items = [];
 
